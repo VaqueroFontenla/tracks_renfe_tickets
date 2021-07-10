f74f4d0f9141c37e4496fce481ae8da97b2f661a
VaqueroFontenla <loret.vaquero@gmail.com>
2021-07-10 17:22:18 +0200

    Elimino comentarios innecesarios

--- index.js
+++ index.js
@@ -2,22 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const startTracking = require("./src/startTracking");
 const tickets = require("./src/data/tickets.json");
+const staticappPath = "./public/dist";
 
-// create app
 const app = express();
 
-// set express middleware
 app.use(express.json());
 app.use(cors());
 
-// init express aplication
 const appPort = 3000;
 app.listen(appPort, () => {
   console.log(`app listening at http://localhost:${appPort}`);
 });
 
-// static app
-const staticappPath = "./public/dist";
 app.use(express.static(staticappPath));
 
 app.get("/updated-tickets", async (req, res) => {
